838cb97c1868f961bc432b5a1a883470e2f34bda
yuchong-pan <panyuchong@gmail.com>
2017-09-16 02:00:23 -0700

    Fixed a typo & added a simple interpreter for arithmetic expression

--- ae.rkt
+++ ae.rkt
@@ -13,9 +13,22 @@
 (define (parse sexp)
   (cond
     [(number? sexp) (num sexp)]
-    [(and (list? sexp) (= (length sexp 2)))
+    [(and (list? sexp) (= (length sexp) 3))
      (case (first sexp)
        [(+) (add (parse (second sexp))
                  (parse (third sexp)))]
        [(-) (sub (parse (second sexp))
                  (parse (third sexp)))])]))
+
+;; calc: AE -> number
+;; consumes an AE and computes the corresponding number
+
+(define (calc an-ae)
+  (type-case AE an-ae
+    [num (n) n]
+    [add (l r) (+ (calc l) (calc r))]
+    [sub (l r) (- (calc l) (calc r))]))
+
+(test (calc (parse '3)) 3)
+(test (calc (parse '{+ 3 4})) 7)
+(test (calc (parse '{+ {- 3 4} 7})) 6)
