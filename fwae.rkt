f496eb7adf1d0917a1cea3b9ad9f6d7af9483b6d
yuchong-pan <panyuchong@gmail.com>
2017-09-17 21:31:14 -0700

    Fixed pre-processor

--- fwae.rkt
+++ fwae.rkt
@@ -121,16 +121,45 @@
 (test (interp (parse '{{{fun {x} x} {fun {x} {+ x 5}}} 3})) (num 8))
 (test (interp (parse '{with {x 3} {fun {y} {+ x y}}})) (fun 'y (add (num 3) (id 'y))))
 
-;; pre-process : FWAE -> FWAE
-(define (pre-process expr)
-  (type-case FWAE expr
-    [num (n) expr]
-    [add (l r) expr]
-    [with (bound-id named-expr bound-body)
-          (app (fun bound-id bound-body) named-expr)]
-    [id (v) expr]
-    [fun (bound-id bound-body) expr]
-    [app (fun-expr arg-expr) expr]))
+;; pre-process : sexp -> sexp
+(define (pre-process sexp)
+  (cond [(number? sexp) sexp]
+        [(and (list? sexp)
+              (= (length sexp) 3)
+              (eq? (first sexp) '+))
+         (list '+
+               (pre-process (second sexp))
+               (pre-process (third sexp)))]
+        [(and (list? sexp)
+              (= (length sexp) 3)
+              (eq? (first sexp) 'with)
+              (list? (second sexp))
+              (= (length (second sexp)) 2)
+              (symbol? (first (second sexp))))
+         (list (list 'fun
+                     (list (first (second sexp)))
+                     (pre-process (third sexp)))
+               (pre-process (second (second sexp))))]
+        [(symbol? sexp) sexp]
+        [(and (list? sexp)
+              (= (length sexp) 3)
+              (eq? (first sexp) 'fun)
+              (list? (second sexp))
+              (= (length (second sexp)) 1)
+              (symbol? (first (second sexp))))
+         (list 'fun
+               (list (first (second sexp)))
+               (pre-process (third sexp)))]
+        [(and (list? sexp)
+              (= (length sexp) 2))
+         (list (pre-process (first sexp))
+               (pre-process (second sexp)))]))
 
-(test (pre-process (parse '{with {x 3} {+ x x}}))
-      (app (fun 'x (add (id 'x) (id 'x))) (num 3)))
+(test (pre-process '3) '3)
+(test (pre-process '{+ 3 {+ 4 x}}) '{+ 3 {+ 4 x}})
+(test (pre-process '{with {x 3} {+ x x}}) '{{fun {x} {+ x x}} 3})
+(test (pre-process 'x) 'x)
+(test (pre-process '{fun {x} {with {y {+ x x}} {+ y y}}})
+      '{fun {x} {{fun {y} {+ y y}} {+ x x}}})
+(test (pre-process '{with {x 1} {{fun {y} {+ y x}} 3}})
+      '{{fun {x} {{fun {y} {+ y x}} 3}} 1})
