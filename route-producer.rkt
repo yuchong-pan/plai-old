a78f2c30c5f2b86ec5fe531dafa0f3497aae5a8c
yuchong-pan <panyuchong@gmail.com>
2017-10-29 12:12:31 -0700

    Added route-producer

---
+++ route-producer.rkt
@@ -0,0 +1,17 @@
+#lang plai
+
+(define route-producer
+  (local [(define resume (box false))]
+    (lambda (real-send)
+      (local [(define send (lambda (value-to-send)
+                             (let/cc k
+                               (begin (set-box! resume k)
+                                      (real-send value-to-send)))))]
+        (if (unbox resume)
+            ((unbox resume) 'dummy)
+            (begin
+              (send 'providence)
+              (send 'houston)
+              (send 'bangalore)))))))
+
+(define get call/cc)
